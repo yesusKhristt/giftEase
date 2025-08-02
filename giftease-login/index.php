369e96b5e03d8ef56fca8b44ef741a93692e0961
U.L.T. Ranasinghe <2023cs156@stu.ucsc.cmb.ac.lk>
2025-08-02 13:01:34 +0530

    Update index.php

--- giftease-login/index.php
+++ giftease-login/index.php
@@ -1,8 +1,6 @@
 <?php
 require_once 'controllers/AuthController.php';
-//poop
-$int = 'poop';
-$boop = 'bup';
+
 $auth = new AuthController();
 
 $action = $_GET['action'] ?? 'login';
@@ -28,4 +26,5 @@ switch ($action) {
                 break;
         }
         break;
+
 }
