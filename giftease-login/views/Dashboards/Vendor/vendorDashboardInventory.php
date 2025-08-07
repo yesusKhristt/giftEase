8eba2e9329eabc4d4a30ccfe19b7513dca1ddcd9
U.L.T. Ranasinghe <2023cs156@stu.ucsc.cmb.ac.lk>
2025-08-07 10:51:40 +0530

    foop

--- giftease-login/views/Dashboards/Vendor/vendorDashboardInventory.php
+++ giftease-login/views/Dashboards/Vendor/vendorDashboardInventory.php
@@ -39,7 +39,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="?action=dashboard&type=vendor&level=messages" class="nav-item">
+                        <a href="?action=dashboard&type=vendor&level=messeges" class="nav-item">
                             <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z">
