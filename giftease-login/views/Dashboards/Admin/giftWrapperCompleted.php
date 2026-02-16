83bd2c1a14699ead2b8147747de5bbd5cc32e80c
SanethTharushika <sanethsiriwardhana@gmail.com>
2026-02-16 11:17:46 +0530

    <link rel="icon" type="image/png" href="resources/1.png"> add this logo to all admins views pages

--- giftease-login/views/Dashboards/Admin/giftWrapperCompleted.php
+++ giftease-login/views/Dashboards/Admin/giftWrapperCompleted.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Gift Wrapper Completed Orders - GiftEase</title>
   <link rel="stylesheet" href="public/style.css">
+  <link rel="icon" type="image/png" href="resources/1.png">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
--- giftease-login/views/Dashboards/Admin/vendors.php
+++ giftease-login/views/Dashboards/Admin/vendors.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Vendor Analysis</title>
     <link rel="stylesheet" href="public/style.css">
+    <link rel="icon" type="image/png" href="resources/1.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
